550c0138fce217493c155b377d28797ddcb04952
Tanya Kunakh <tkunah@gmail.com>
2025-06-13 11:35:04 +0300

    after ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,35 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [FormsModule, HttpClientModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  text:string = '';
-  wordsQuantity:number = 0;
-  symbolsQuantity:number = 0;
-  selectedText:string = '';
+  public text:string = '';
+  public wordsQuantity:number = 0;
+  public symbolsQuantity:number = 0;
+  private selectedText:string = '';
+  public synList:any = [];
+  private synUrl:string = 'https://api.datamuse.com/words?rel_syn=';
+  private http = inject(HttpClient);
+  private changeDetector = inject(ChangeDetectorRef);
+  private subscription: Subscription = new Subscription();
   @ViewChild('textArea') textareaRef!: ElementRef<HTMLTextAreaElement>;
-  synList:any = [];
-  synUrl:string = 'https://api.datamuse.com/words?rel_syn=';
-  constructor(private http: HttpClient) {}
 
-  onTextareaChange() {
+  public onTextareaChange() {
     const curText = this.text;
 
     this.symbolsQuantity = curText.split(' ').join("").length; //curText.length;
     this.wordsQuantity = curText.trim().split(/\s+/).length;
   }
 
-  getSelectedText(textarea: HTMLTextAreaElement): void {
+  public getSelectedText(textarea: HTMLTextAreaElement): void {
     const start = textarea.selectionStart;
     const end = textarea.selectionEnd;
 
@@ -36,7 +40,7 @@ export class AppComponent {
     }
   }
 
-  addTextSelection() {
+  private addTextSelection() {
     const start = this.text.indexOf(this.selectedText);
     const end = this.selectedText.length;
 
@@ -44,31 +48,31 @@ export class AppComponent {
     this.textareaRef.nativeElement.setSelectionRange(start, start+end);
   }
 
-  synonymFind() {
+  public synonymFind() {
     const curText = this.selectedText.split(' ').join("+");
 
-    this.http.get(this.synUrl+curText).subscribe(
+    this.subscription = this.http.get(this.synUrl+curText).subscribe(
       res => this.synList = res,
       err => console.error(err),
       () => {
         console.log('HTTP request completed.', this.synList);
+        this.subscription.unsubscribe();
         this.addTextSelection();
       });
   }
 
-  copyText(textarea: HTMLTextAreaElement): void {
+  public copyText(textarea: HTMLTextAreaElement): void {
     navigator.clipboard.writeText(textarea.value).then(() => {
       textarea.select();
       console.log(textarea.value);
     }).catch(err => console.error(err));
   }
 
-  changeWord(word: string) {
+  public changeWord(word: string) {
     this.text = this.text.replace(this.selectedText, word);
     this.selectedText = word;
-    setTimeout(() => {
-      this.addTextSelection();
-      this.onTextareaChange();
-    }, 0);
+    this.addTextSelection();
+    this.onTextareaChange();
+    this.changeDetector.detectChanges();
   }
 }
